9ebed28ed81007d9047189021332737b6e3562b4
1010mark <markmentaro@yahoo.co.jp>
2024-06-28 18:44:13 +0900

    some cors setting

--- app/api/post/route.tsx
+++ app/api/post/route.tsx
@@ -9,7 +9,15 @@ export async function POST(request: NextRequest) {
    ,lang: "jp"
    ,interval:"1h"
   };
+  const corsHeaders = {
+    'Access-Control-Allow-Origin': process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://stockprice-to-sound.vercel.app/", // 許可するオリジン
+    'Access-Control-Allow-Methods': 'POST', // 許可するメソッド
+    'Access-Control-Allow-Headers': 'Content-Type', // 許可するリクエストヘッダー
+  }
   const result = await yahooFinance.chart(`${body.stockcode}.T`, queryOptions);
-  let res = new Response(JSON.stringify({ quotes: result.quotes.slice(-40) }));
+  let res = new Response(JSON.stringify({ quotes: result.quotes.slice(-40) }), {
+    status: 200,
+    headers: corsHeaders
+  });
   return res;
 }
